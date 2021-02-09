a399636b0768019f3e3e36224d8b53e53a2ec37d
calavell <calavell@tcd.ie>
2021-02-09 16:19:13 +0000

    added test for empty statement

--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -11,16 +11,22 @@ describe StatementPrinter do
     '10/02/2021 ||  || 2 || 8'
   ]
 
+  empty_answer = ['date || credit || debit || balance']
+
   let(:printer) { described_class.new }
   let(:transaction1) { double(Transaction, date: '08/02/2021', credit: 5, debit: nil, balance: 5) }
   let(:transaction2) { double(Transaction, date: '09/02/2021', credit: 5, debit: nil, balance: 10) }
   let(:transaction3) { double(Transaction, date: '10/02/2021', credit: nil, debit: 2, balance: 8) }
   let(:transaction_list) { [transaction1, transaction2, transaction3] }
   let(:bank_statement) { double(BankStatement, transactions: transaction_list) }
-
+  let(:empty_bank_statement) { double(BankStatement, transactions: []) }
   describe '#print_statement' do
       it 'prints off a statement in the correct format' do
         expect(printer.print_bank_statement(bank_statement)).to eq(answer)
+      end
+
+      it 'prints off a statement in the correct format when there are no transaction' do
+        expect(printer.print_bank_statement(empty_bank_statement)).to eq(empty_answer)
       end
   end
 end
